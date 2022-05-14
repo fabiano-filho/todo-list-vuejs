421dce94fbe390614034e4eae65a3032cf806228
Fabiano Filho <fabianobnfilho09@gmail.com>
2022-05-13 23:08:08 -0300

    criando botoes editar e concluir

--- src/components/FormTask.vue
+++ src/components/FormTask.vue
@@ -1,36 +1,9 @@
 <template>
   <div class="form-task">
       <input @keypress="keyPress($event)" v-model="task" type="text" placeholder="Digite sua tarefa" class="text-task">
-      <button  @click.prevent="addTask" class="button">Adicionar</button>
+      <button  @click.prevent="addTask" class="button">{{buttonText}}</button>
   </div>
 </template>
-
-<style scoped>
-.form-task {
-    display: flex;
-    gap: 2rem;
-    justify-content: space-between;
-    align-items: center;
-    margin-block: 3rem;
-}
-
-.text-task {
-    width: 100%;
-    padding: 0.9rem 1.6rem;
-    outline: none;
-    border: 1px solid cornflowerblue;
-    border-radius: .6rem;
-}
-
-.button {
-    padding: 0.9rem 1.6rem;
-    background-color: rgb(0, 108, 250);
-    color: white;
-    border: none;
-    border-radius: .6rem;
-    cursor: pointer;
-}
-</style>
 
 <script>
 export default {
@@ -39,6 +12,9 @@ export default {
       return {
           task: ''
       }
+  },
+  props: {
+      buttonText: String,
   },
   methods: {
       addTask(){
